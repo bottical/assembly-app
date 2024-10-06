286457cb431616f83d91c38f94210944f454bfdc
bottical <129402269+bottical@users.noreply.github.com>
2024-10-06 17:32:09 +0900

    Update inspection_process.js

--- inspection_process.js
+++ inspection_process.js
@@ -111,12 +111,3 @@ window.onload = function() {
     }
   });
 }
-
-  // Enterキーでバーコードを入力できるように設定
-  document.getElementById('barcodeInput').addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-      event.preventDefault(); // Enterキーのデフォルト動作を防ぐ
-      checkBarcode(); // Enterキーでバーコード検品を実行
-    }
-  });
-}
