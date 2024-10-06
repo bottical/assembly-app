a75975623e5d4470390f3e6cf40a1d83244579b0
bottical <129402269+bottical@users.noreply.github.com>
2024-10-06 16:40:35 +0900

    Create firebase_setup.js

---
+++ firebase_setup.js
@@ -0,0 +1,18 @@
+// Firebase設定とFirestoreインスタンス作成
+if (!window.db) { // すでにdbが定義されているか確認
+  const firebaseConfig = {
+    apiKey: "AIzaSyAr7u9B-w9XXdWbEtWwwvjJhuDKDuecjmM",
+    authDomain: "assembly-app-b644d.firebaseapp.com",
+    projectId: "assembly-app-b644d",
+    storageBucket: "assembly-app-b644d.appspot.com",
+    messagingSenderId: "854507856467",
+    appId: "1:854507856467:web:c028af6a65afb9a1901ca6"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  // Firestoreインスタンスを作成してグローバルに保存
+  window.db = firebase.firestore();
+}
